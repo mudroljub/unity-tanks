6ae01d79efa47f3cf317ee05a3828840d4b5959f
Damjan Pavlica <mudroljub@gmail.com>
2020-03-19 20:33:16 +0100

    obrisane praznine

--- Assets/Scripts/Tank/TankHealth.cs
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -3,65 +3,59 @@ using UnityEngine.UI;
 
 public class TankHealth : MonoBehaviour
 {
-  public float m_StartingHealth = 100f;               // The amount of health each tank starts with.
-  public Slider m_Slider;                             // The slider to represent how much health the tank currently has.
-  public Image m_FillImage;                           // The image component of the slider.
-  public Color m_FullHealthColor = Color.green;       // The color the health bar will be when on full health.
-  public Color m_ZeroHealthColor = Color.red;         // The color the health bar will be when on no health.
-  public GameObject m_ExplosionPrefab;                // A prefab that will be instantiated in Awake, then used whenever the tank dies.
+    public float m_StartingHealth = 100f;
+    public Slider m_Slider;                             // The slider to represent how much health the tank currently has.
+    public Image m_FillImage;                           // The image component of the slider.
+    public Color m_FullHealthColor = Color.green;
+    public Color m_ZeroHealthColor = Color.red;
+    public GameObject m_ExplosionPrefab;
 
+    private AudioSource m_ExplosionAudio;
+    private ParticleSystem m_ExplosionParticles;
+    private float m_CurrentHealth;
+    private bool m_Dead;
 
-  private AudioSource m_ExplosionAudio;               // The audio source to play when the tank explodes.
-  private ParticleSystem m_ExplosionParticles;        // The particle system the will play when the tank is destroyed.
-  private float m_CurrentHealth;                      // How much health the tank currently has.
-  private bool m_Dead;                                // Has the tank been reduced beyond zero health yet?
-
-
-  private void Awake()
-  {
-    // Get a reference to the audio source on the instantiated prefab.
-    m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>();
-    // Instantiate the explosion prefab and get a reference to the particle system on it.
-    m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>();
-    // Disable the prefab so it can be activated when it's required.
-    m_ExplosionParticles.gameObject.SetActive(false);
-  }
-
-  private void OnEnable()
-  {
-    m_CurrentHealth = m_StartingHealth;
-    m_Dead = false;
-    SetHealthUI();
-  }
-
-  public void TakeDamage(float amount)
-  {
-    m_CurrentHealth -= amount;
-    SetHealthUI();
-    if (m_CurrentHealth <= 0f && !m_Dead)
+    private void Awake()
     {
-      OnDeath();
+        // Instantiate the explosion prefab and get a reference to the particle system on it.
+        m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>();
+        // Get a reference to the audio source on the instantiated prefab.
+        m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>();
+        // Disable the prefab so it can be activated when it's required.
+        m_ExplosionParticles.gameObject.SetActive(false);
     }
-  }
 
+    private void OnEnable()
+    {
+        m_CurrentHealth = m_StartingHealth;
+        m_Dead = false;
+        SetHealthUI();
+    }
 
-  private void SetHealthUI()
-  {
-    m_Slider.value = m_CurrentHealth;
-    // Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
-    m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
-  }
+    public void TakeDamage(float amount)
+    {
+        m_CurrentHealth -= amount;
+        SetHealthUI();
+        if (m_CurrentHealth <= 0f && !m_Dead) OnDeath();
+    }
 
-  private void OnDeath()
-  {
-    m_Dead = true;
+    private void SetHealthUI()
+    {
+        m_Slider.value = m_CurrentHealth;
+        // Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
+        m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
+    }
 
-    // Move the instantiated explosion prefab to the tank's position and turn it on.
-    m_ExplosionParticles.transform.position = transform.position;
-    m_ExplosionParticles.gameObject.SetActive(true);
-    m_ExplosionParticles.Play();
-    m_ExplosionAudio.Play();
+    private void OnDeath()
+    {
+        m_Dead = true;
+        // Move the instantiated explosion prefab to the tank's position and turn it on.
+        m_ExplosionParticles.transform.position = transform.position;
+        m_ExplosionParticles.gameObject.SetActive(true);
 
-    gameObject.SetActive(false);
-  }
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+
+        gameObject.SetActive(false);
+    }
 }
